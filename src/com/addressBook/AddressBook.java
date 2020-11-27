1f8245c9193b6503b867b07a17d63a1c075d590a
rishika7-hub <srishika970@gmail.com>
2020-11-27 21:03:35 +0530

    Added

---
+++ src/com/addressBook/AddressBook.java
@@ -0,0 +1,15 @@
+package com.addressBook;
+import java.util.Scanner;
+
+public class AddressBook {
+
+	public static void main(String[] args)  {
+		System.out.println("Welcome to Address Book Problem");
+		AddressMethod address=new AddressMethod();
+		address.Add();
+		address.Show();
+	}
+	}
+		
+		
+			
---
+++ src/com/addressBook/AddressMethod.java
@@ -0,0 +1,28 @@
+package com.addressBook;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class AddressMethod {
+	ArrayList<Address> addressBook = new ArrayList<Address>();
+
+	public void Add() {
+		// TODO Auto-generated method stub
+		Address address = new Address();
+		addressBook.add(address);
+	}
+
+	public void Show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	}
+
+	
